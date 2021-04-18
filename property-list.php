0ca000be9050f781fae08d7bae3f4617ee2ad165
Atul Gadhari <57897523+Atulgadhari27@users.noreply.github.com>
2021-04-18 15:51:45 +0530

    first

---
+++ property-list.php
@@ -0,0 +1,41 @@
+<?php
+    include("config/config.php");
+?>
+
+<div class="smoke">
+        <div class="container">
+            <div class="heading animate-box">
+                <h2><b>All Properties</b></h2>
+            </div>
+            <div class="text-center animate-box">
+                <h3>Rent your dream house</h3>
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-sm-4" data-animate-effect="fadeInLeft">
+                    <div class="price-box animate-box">
+                        <img class="card-img-top" style="height:317px" src="./img/house_images/House_1/1_1.jpg" alt="">
+                        <div>Orchard House</div>
+                        <br>
+                        <a href="property.php?House_No=1"  class="btn btn-banner" >View Property </a>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="price-box animate-box">
+                        <img class="card-img-top" style="height:317px" src="./img/house_images/House_2/2_1.jpg" alt="">
+                        <div>Meadow View</div>
+                        <br>
+                        <a href="property.php?House_No=2"  class="btn btn-banner" >View Property </a>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="price-box animate-box">
+                        <img class="card-img-top" style="height:317px" src="./img/house_images/House_3/3_1.jpg" alt="">
+                        <div>Ivy Cottage</div>
+                        <br>
+                        <a href="property.php?House_No=3"  class="btn btn-banner" >View Property </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
---
+++ team.php
@@ -0,0 +1,93 @@
+    <div class="smoke" >
+        <div class="container" id="team">
+            <div class="heading animate-box">
+                <h2><b>OUR TEAM</b></h2>
+            </div>
+            <div class="row animate-box">
+                <div class="col-md-12">
+                    <div id="quote-carousel" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active item">
+                                <blockquote>
+                                    <div class="row">
+                                        <div class="col-sm-8 offset-sm-2">
+                                            <p class="testi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vero placeat ipsam eligendi modi 
+                                            vel provident nulla ipsum, consequatur cupiditate eos perspiciatis, veritatis assumenda rem.</p>
+                                            <div class="text-center"><i class="fa fa-caret-down down-ar"></i></div>
+                                            <div class="text-center">
+                                                <h4 class="text-org">Atul Gadhari</h4>
+                                                <h5 class="text-dark">Lead Designer</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </blockquote>
+                            </div>
+                            <div class="carousel-item item">
+                                <blockquote>
+                                    <div class="row">
+                                        <div class="col-sm-8 offset-sm-2">
+                                            <p class="testi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vero placeat ipsam eligendi modi 
+                                            vel provident nulla ipsum, consequatur cupiditate eos perspiciatis, veritatis assumenda rem.
+                                            </p>
+                                            <div class="text-center"><i class="fa fa-caret-down down-ar"></i></div>
+                                            <div class="text-center">
+                                                <h4 class="text-org">Rishit Dixit</h4>
+                                                <h5 class="text-dark">Lead Designer</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </blockquote>
+                            </div>
+                            <div class="carousel-item item">
+                                <blockquote>
+                                    <div class="row">
+                                        <div class="col-sm-8 offset-sm-2">
+                                            <p class="testi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vero placeat ipsam eligendi modi 
+                                            vel provident nulla ipsum, consequatur cupiditate eos perspiciatis, veritatis assumenda rem.</p>
+                                            <div class="text-center"><i class="fa fa-caret-down down-ar"></i></div>
+                                            <div class="text-center">
+                                                <h4 class="text-org">Prashil Ganvir</h4>
+                                                <h5 class="text-dark">Lead Designer</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </blockquote>
+                            </div>
+                            <div class="carousel-item item">
+                                <blockquote>
+                                    <div class="row">
+                                        <div class="col-sm-8 offset-sm-2">
+                                            <p class="testi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vero placeat ipsam eligendi modi 
+                                            vel provident nulla ipsum, consequatur cupiditate eos perspiciatis, veritatis assumenda rem.</p>
+                                            <div class="text-center"><i class="fa fa-caret-down down-ar"></i></div>
+                                            <div class="text-center">
+                                                <h4 class="text-org">Tejas Adsare</h4>
+                                                <h5 class="text-dark">Lead Designer</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </blockquote>
+                            </div>
+                        </div>
+
+                        <ol class="carousel-indicators">
+                            <li data-target="#quote-carousel" data-slide-to="0" class="active">
+                                    <img class="img-responsive " src="img/member2.png" alt="">
+                            </li>
+                            <li data-target="#quote-carousel" data-slide-to="1"><img class="img-responsive"
+                                    src="img/member1.jpg" alt="">
+                            </li>
+                            <li data-target="#quote-carousel" data-slide-to="2"><img class="img-responsive"
+                                    src="img/member3.jpg" alt="">
+                            </li>
+                            <li data-target="#quote-carousel" data-slide-to="3"><img class="img-responsive"
+                                    src="img/member4.jpg" alt="">
+                            </li>
+                        </ol>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
